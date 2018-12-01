d1bf9b18ddff3aefed3651c8cb7e5de947383cf9
Timur <timur.ivanov.2002@mail.ru>
2018-12-01 15:44:08 +0500

    Правки

--- js/main.js
+++ js/main.js
@@ -39,13 +39,13 @@ $(document).ready(function() {
         phoneVal = $('[name = user_phone]').val();
 
     $.ajax({
-      url: '../mailer/smart.php',
+      url: 'mailer/smart.php',
       data: {name:nameVal, phone:phoneVal},
       success: function() {
         // $('.class').fadeIn();
-        $(this).find('input').val('');
+        // $(this).find('input').val('');
         alert('Сообщение успешно отправлено');
-        $('form').trigger('reset');
+        // $('form').trigger('reset');
       }
     });
     return false;
